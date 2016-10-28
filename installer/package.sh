b7adaa3ee79eb3a9a13d0a3e6d8d30ed31c82a87
Jason Champion <jchampion@zetacentauri.com>
2016-10-27 23:07:22 -0700

    OSX packaging changes.

--- installer/package.sh
+++ installer/package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-codesign --deep -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" MidiPlayer.app --entitlements MidiPlayer.app/Contents/Resources/Entitlements.plist
-productbuild --sign "3rd Party Mac Developer Installer: Jason Champion (G6PFF85X2R)" --component MidiPlayer.app /Applications MidiPlayer.pkg
+codesign --deep -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" MIDIPlayer.app --entitlements MIDIPlayer.app/Contents/Resources/Entitlements.plist
+productbuild --sign "3rd Party Mac Developer Installer: Jason Champion (G6PFF85X2R)" --component MIDIPlayer.app /Applications MIDIPlayer.pkg
