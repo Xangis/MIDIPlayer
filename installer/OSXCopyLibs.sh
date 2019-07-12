30ec2444dd680589b91cee3dd0bd9d760dbe7af4
Jason Champion <xangis@ip-192-168-0-36.us-west-2.compute.internal>
2019-07-12 01:11:14 -0700

    Utility script to copy wxWidgets libraries into app directory.`

---
+++ installer/OSXCopyLibs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This should only need to be run once to get the wxWidgets libraries into the app directory.
+cp /usr/local/lib/libwx_osx_cocoau_adv-3.1.dylib MidiPlayer.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_osx_cocoau_core-3.1.dylib MidiPlayer.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_osx_cocoau_html-3.1.dylib MidiPlayer.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_baseu_net-3.1.dylib MidiPlayer.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_baseu_net-3.1.dylib MidiPlayer.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_osx_cocoau_qa-3.1.dylib MidiPlayer.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_baseu_xml-3.1.dylib MidiPlayer.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_osx_cocoau_xrc-3.1.dylib MidiPlayer.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_baseu-3.1.dylib MidiPlayer.app/Contents/Frameworks/
